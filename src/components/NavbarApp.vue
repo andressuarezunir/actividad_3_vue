bfc41c371a868821dee79bf098849561ea24dae9
Andrés Suárez <andressuarez@ticsocial.com.co>
2024-06-17 09:54:21 -0500

    Navbar component added to page to change between views, remove unnecessary files

---
+++ src/components/NavbarApp.vue
@@ -0,0 +1,34 @@
+<script setup lang="ts">
+import { RouterLink } from 'vue-router'
+</script>
+
+<template>
+  <nav class="navbar navbar-expand-lg navbar-dark bg-dark">
+    <div class="container px-5">
+      <a class="navbar-brand" href="index.html">
+        <img src="../assets/img/logoB.png" class="w-75" alt="log" />
+      </a>
+      <button
+        class="navbar-toggler"
+        type="button"
+        data-bs-toggle="collapse"
+        data-bs-target="#navbarSupportedContent"
+        aria-controls="navbarSupportedContent"
+        aria-expanded="false"
+        aria-label="Toggle navigation"
+      >
+        <span class="navbar-toggler-icon"></span>
+      </button>
+      <div class="collapse navbar-collapse" id="navbarSupportedContent">
+        <ul class="navbar-nav ms-auto mb-2 mb-lg-0">
+          <li class="nav-item">
+            <RouterLink class="nav-link" to="/">Inicio</RouterLink>
+          </li>
+          <li class="nav-item"><RouterLink class="nav-link" to="/form">Formulario</RouterLink></li>
+        </ul>
+      </div>
+    </div>
+  </nav>
+</template>
+
+<style scoped></style>
