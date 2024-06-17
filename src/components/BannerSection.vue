7de172260bc3a1e9b3317885ac9c8170a8735012
Andrés Suárez <andressuarez@ticsocial.com.co>
2024-06-17 10:57:02 -0500

    BannerSection component added

---
+++ src/components/BannerSection.vue
@@ -0,0 +1,18 @@
+<script setup lang="ts">
+const text =
+  'Que hermosa eres imperial Cartagena, coronada de murallas y antiguos balcones, con tu majestuosidad y señorío. Que a los turistas robas sus corazones.'
+</script>
+
+<template>
+  <section class="container px-5 pt-3 pb-5">
+    <aside class="bg-primary bg-gradient rounded-3 p-4 p-sm-5 mt-5">
+      <div
+        class="d-flex align-items-center justify-content-between flex-column flex-xl-row text-center text-xl-start"
+      >
+        <div class="mb-4 mb-xl-0">
+          <div class="fs-3 fw-bold text-white">"{{ text }}"</div>
+        </div>
+      </div>
+    </aside>
+  </section>
+</template>
--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -2,12 +2,14 @@
 import HeroSection from '../components/HeroSection.vue'
 import FeaturesSection from '../components/FeaturesSection.vue'
 import TestimonialSection from '../components/TestimonialSection.vue'
+import BannerSection from '../components/BannerSection.vue'
 </script>
 
 <template>
   <HeroSection />
   <FeaturesSection />
   <TestimonialSection />
+  <BannerSection />
 </template>
 
 <style></style>
